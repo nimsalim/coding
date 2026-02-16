3a12ee189d1d2a77f2b7951ac66dc1db1f709f38
Souleymane <nimaga.isc@gmail.com>
2026-02-15 19:10:52 -0500

    git

---
+++ wk7/testfile.py
@@ -0,0 +1,14 @@
+# print hello world
+print("HW")
+
+
+def calculate_average(numbers)
+    total = sum(numbers)
+    count = len(numbers
+    average = total / count
+    return average
+
+my_list = [1, 2, 3, 4, 5]
+result = calculate_average(my_list)
+print("The average is: " + result)
+
